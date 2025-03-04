e1792454b3cc96f9bb895dda1e3b266fc8a4a21d
Dustyn-T-Lewis <dtl0018@auburn.edu>
2025-03-04 17:58:49 -0600

    second commit

--- Formatting.R
+++ Formatting.R
@@ -1,23 +1,18 @@
-#     Loading the data "stringAsFactors" function ensures that text (character) data are not converted
+# Loading the data "stringAsFactors" function ensures that text (character) data are not converted
 #     into categorical factors automatically which could make string manipulations harder
 getwd()
 data <- read.csv("Master Proteomics.csv", stringsAsFactors = FALSE)
 colnames(data)
+
+# Load Required Libraries
 library(dplyr)
 library(stringr)
 
-
-#     Installing biomaRt package to fetch accession numbers from Ensemble database
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("biomaRt")
-
-library(biomaRt)
-library(biomartr)
-
-ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl", host = "https://useast.ensembl.org")
-
+# Install and Load `rentrez` for UniProt Queries
+if (!requireNamespace("rentrez", quietly = TRUE)) {
+  install.packages("rentrez")
+}
+library(rentrez)
 
 # Extracting Gene names from Gene_Proteins
 gene_name_extract <- function(Gene_Proteins) {
@@ -27,7 +22,6 @@ gene_name_extract <- function(Gene_Proteins) {
     return(str_split(Gene_Proteins, "_")[[1]][1])
   }
 }
-
 
 # Extracting Gene descriptions from Gene_Proteins
 gene_description_extract <- function(Gene_Proteins) {
@@ -43,17 +37,15 @@ gene_description_extract <- function(Gene_Proteins) {
   }
 }
 
-
-# Extracting Accession Numbers from Gene_Proteins
+# Extracting UniProt Accession Numbers from Gene_Proteins
 accession_extract <- function(Gene_Proteins) {
   accession <- str_extract_all(Gene_Proteins, "[OPQ][0-9][A-Z0-9]{3}[0-9]")  
   if (length(accession[[1]]) > 0) {  
-    return(accession[[1]][1])  # Return the first match
+    return(accession[[1]][1])  # Return the first UniProt Accession match
   } else {
     return(NA)  
   }
 }
-
 
 # Inserting gene names, descriptions, and accessions into table
 proteomics_data <- data %>%
@@ -61,58 +53,71 @@ proteomics_data <- data %>%
          Gene_Descriptions = sapply(Gene_Proteins, gene_description_extract),
          Accession = sapply(Gene_Proteins, accession_extract))
 
-
-#There are two issues at this point: 
+# There are two issues at this point: 
 # 1) Some rows have gene names but no accession number,
 # 2) Some rows have accession numbers but no gene names
-#BiomaRt can use gene names used to retrieve accessions and accessions can be used to retrieve gene names
+# UniProt can use gene names to retrieve accessions and accessions to retrieve gene names
 
-#1 getting accession
+# Function to Query UniProt for Accession Numbers by Gene Name
 get_accession_by_gene <- function(Gene_Names) {
-  # 1. If the gene name is missing, empty, or just "GN=", return NA
   if (is.na(Gene_Names) || Gene_Names == "" || Gene_Names == "GN=") {
     return(NA)
   }
   
-  # 2. Query Ensembl using biomaRt to find the corresponding accession number
-  result <- getBM(attributes = c("hgnc_symbol", "ensembl_gene_id"),  # What to return; 'getBM' searches for accession using gene name
-                  filters = "hgnc_symbol",  # Search by gene name
-                  values = Gene_Names,  # The gene name to look up
-                  mart = ensembl)  # Ensembl database connection
+  query <- paste(Gene_Names, "[GENE] AND Homo sapiens[ORGN]")
+  result <- entrez_search(db = "protein", term = query)
   
-  # 3. If a match is found, return the first accession number
-  if (nrow(result) > 0) {
-    return(result$ensembl_gene_id[1])  # Take the first result
+  if (!is.null(result) && length(result$ids) > 0) {
+    return(result$ids[1])  # Return the first UniProt Accession match
   } else {
-    return(NA)  # If no match, return NA
+    return(NA)
   }
 }
 
-#2 getting gene
+# Function to Query UniProt for Gene Name by Accession Number
 get_gene_by_accession <- function(accession) {
   if (is.na(accession) || accession == "") {
     return(NA)
   }
   
-  result <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"),
-                  filters = "ensembl_gene_id",
-                  values = accession,
-                  mart = ensembl)
+  result <- entrez_summary(db = "protein", id = accession)
+  gene_name <- result$organism
   
-  if (nrow(result) > 0) {
-    return(result$hgnc_symbol[1])  # Return the first match
+  if (!is.null(gene_name)) {
+    return(gene_name)  # Return the gene name
   } else {
     return(NA)
   }
 }
 
-#mutate proteomics to add data
-proteomics_data <- proteomics_data %>%
-  mutate(
-    Accession = ifelse(is.na(Accession) | Accession == "", 
-                       sapply(Gene_Names, get_accession_by_gene), 
-                       Accession),
-    Gene_Names = ifelse(is.na(Gene_Names) | Gene_Names == "GN=", 
-                        sapply(Accession, get_gene_by_accession), 
-                        Gene_Names)
+# Mutating Proteomics Data Using Batches to Avoid API Limits
+batch_size <- 100  # Adjust based on your dataset size
+num_batches <- ceiling(nrow(proteomics_data) / batch_size)
+
+for (i in 1:num_batches) {
+  start <- (i - 1) * batch_size + 1
+  end <- min(i * batch_size, nrow(proteomics_data))
+  
+  proteomics_data[start:end, "Accession"] <- ifelse(
+    is.na(proteomics_data[start:end, "Accession"]) | proteomics_data[start:end, "Accession"] == "",
+    sapply(proteomics_data[start:end, "Gene_Names"], get_accession_by_gene),
+    proteomics_data[start:end, "Accession"]
   )
+  
+  proteomics_data[start:end, "Gene_Names"] <- ifelse(
+    is.na(proteomics_data[start:end, "Gene_Names"]) | proteomics_data[start:end, "Gene_Names"] == "GN=",
+    sapply(proteomics_data[start:end, "Accession"], get_gene_by_accession),
+    proteomics_data[start:end, "Gene_Names"]
+  )
+}
+
+# Save Processed Data to CSV
+save_proteomics_data <- function(data, filename = "Processed_Proteomics_Data.csv") {
+  write.csv(data, file = filename, row.names = FALSE)
+  message(paste("✅ Data successfully saved to:", filename))
+}
+
+# Call the function to save your data
+save_proteomics_data(proteomics_data)
+
+View(proteomics_data)
